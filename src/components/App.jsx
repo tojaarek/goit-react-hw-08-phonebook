6339e677af2f3611ab2ec4f9d1d2bd67d00bd295
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-09-01 12:06:02 +0200

    deleted lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import ContactsPage from 'pages/ContactsPage/ContactsPage';
-import Layout from 'layouts/layout';
+import Layout from './Layouts/Layout';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/actions';
--- src/const/breakepoints.js
+++ src/const/breakepoints.js
@@ -4,7 +4,7 @@ const breakpoints = {
   md: '768px',
   lg: '1024px',
   xl: '1280px',
-  xxl: '1400px',
+  xxl: '1536px',
 };
 
 export const devices = {
