4e54c7ad74cee3f18f461c30d313da6fffdcf6b4
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-09-04 15:05:41 +0200

    mobile menu

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link as link } from 'react-router-dom';
+import { devices } from 'const/breakepoints';
 
 export const Container = styled.div`
   width: 100%;
@@ -9,39 +10,50 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.header`
-  padding: 30px 20px;
+  padding: 20px 20px;
+  @media only screen and ${devices.md} {
+    padding: 30px 20px;
+  }
 `;
 
 export const Login = styled(link)`
-  padding: 8px 16px;
-  border-radius: 4px;
-  border: 2px solid transparent;
-  text-decoration: none;
-  color: #fbfbfb;
-  font-size: 16px;
-  font-weight: 500;
-  transition: 200ms cubic-bezier(0.17, 0.67, 0.83, 0.67);
-  transition-property: color, border;
-  margin-right: 40px;
-  &:is(:focus, :hover) {
-    color: #ff8a05;
+  display: none;
+  @media only screen and ${devices.md} {
+    display: inline;
+    padding: 8px 16px;
+    border-radius: 4px;
+    border: 2px solid transparent;
+    text-decoration: none;
+    color: #fbfbfb;
+    font-size: 16px;
+    font-weight: 500;
+    transition: 200ms cubic-bezier(0.17, 0.67, 0.83, 0.67);
+    transition-property: color, border;
+    margin-right: 40px;
+    &:is(:focus, :hover) {
+      color: #ff8a05;
+    }
   }
 `;
 
 export const SignUp = styled(link)`
-  padding: 8px 16px;
-  border-radius: 4px;
-  border: 2px solid #ff8a05;
-  text-decoration: none;
-  color: #ff8a05;
-  font-size: 16px;
-  font-weight: 500;
-  transition: 200ms cubic-bezier(0.17, 0.67, 0.83, 0.67);
-  transition-property: color, border;
-  &:is(:focus, :hover) {
-    color: #262626;
+  display: none;
+  @media only screen and ${devices.md} {
+    display: inline;
+    padding: 8px 16px;
+    border-radius: 4px;
     border: 2px solid #ff8a05;
-    background-color: #ff8a05;
+    text-decoration: none;
+    color: #ff8a05;
+    font-size: 16px;
+    font-weight: 500;
+    transition: 200ms cubic-bezier(0.17, 0.67, 0.83, 0.67);
+    transition-property: color, border;
+    &:is(:focus, :hover) {
+      color: #262626;
+      border: 2px solid #ff8a05;
+      background-color: #ff8a05;
+    }
   }
 `;
 
@@ -73,4 +85,106 @@ export const Logo = styled(link)`
 export const NavLoggedBox = styled.div`
   display: flex;
   align-items: center;
+`;
+
+export const MobileButton = styled.div`
+  height: 25px;
+  width: 35px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #262626;
+  cursor: pointer;
+  position: relative;
+  @media only screen and ${devices.md} {
+    display: none;
+  }
+`;
+
+export const MobileButtonOpen = styled.div`
+  height: 3px;
+  border-radius: 5px;
+  width: 100%;
+  background-color: #fbfbfb;
+  @media only screen and ${devices.md} {
+    display: none;
+  }
+`;
+
+export const MobileButtonClose1 = styled.div`
+  height: 3px;
+  border-radius: 5px;
+  width: 100%;
+  background-color: #fbfbfb;
+  position: absolute;
+  top: 50%;
+  transform: rotate(45deg);
+  @media only screen and ${devices.md} {
+    display: none;
+  }
+`;
+
+export const MobileButtonClose2 = styled.div`
+  height: 3px;
+  border-radius: 5px;
+  width: 100%;
+  background-color: #fbfbfb;
+  position: absolute;
+  top: 50%;
+  transform: rotate(-45deg);
+  @media only screen and ${devices.md} {
+    display: none;
+  }
+`;
+
+export const MobileMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #353535;
+  width: 100%;
+  position: absolute;
+  top: 50px;
+  left: 0;
+  padding: 40px 0;
+  list-style: none;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  @media only screen and ${devices.md} {
+    display: none;
+  }
+`;
+
+export const MobileMenuItem = styled.li`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  &:not(:last-child) {
+    margin-bottom: 40px;
+  }
+`;
+
+export const MobileLogin = styled(link)`
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  padding: 8px 0;
+  border-radius: 4px;
+  border: 2px solid #ff8a05;
+  text-decoration: none;
+  color: #ff8a05;
+  font-size: 16px;
+  font-weight: 500;
+  transition: 200ms cubic-bezier(0.17, 0.67, 0.83, 0.67);
+  transition-property: color, border;
+  &:is(:focus, :hover) {
+    color: #262626;
+    border: 2px solid #ff8a05;
+    background-color: #ff8a05;
+  }
+`;
+
+export const Redirect = styled(link)`
+  color: #ff8a05;
 `;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,55 @@
-import { SignUp, Login } from 'components/AppBar/AppBar.styled';
+import {
+  SignUp,
+  Login,
+  MobileButton,
+  MobileButtonOpen,
+  MobileButtonClose1,
+  MobileButtonClose2,
+  MobileMenu,
+  MobileMenuItem,
+  MobileLogin,
+  Redirect,
+} from 'components/AppBar/AppBar.styled';
+import { useState } from 'react';
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [mobileButtonOpen, setMobileButtonOpen] = useState(true);
+
+  const handleMobileButtonClick = () => {
+    setMobileButtonOpen(prevState => !prevState);
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav>
       <Login to="/login">Log in</Login>
       <SignUp to="/register">Sign up</SignUp>
+      <MobileButton onClick={handleMobileButtonClick}>
+        {mobileButtonOpen ? (
+          <>
+            <MobileButtonOpen />
+            <MobileButtonOpen />
+            <MobileButtonOpen />
+          </>
+        ) : (
+          <>
+            <MobileButtonClose1 />
+            <MobileButtonClose2 />
+          </>
+        )}
+      </MobileButton>
+      {menuOpen && (
+        <MobileMenu>
+          <MobileMenuItem>
+            <MobileLogin to="/login">Log in</MobileLogin>
+          </MobileMenuItem>
+          <MobileMenuItem>
+            Don't have an account yet?
+            <Redirect to="/register">Sign up</Redirect>
+          </MobileMenuItem>
+        </MobileMenu>
+      )}
     </nav>
   );
 };
